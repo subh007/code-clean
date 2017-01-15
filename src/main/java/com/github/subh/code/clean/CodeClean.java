e960acefce8bcc4ddd8813f0beb861a896c34598
subhash kumar singh <subh.singh007@gmail.com>
2017-01-15 20:13:09 +0530

    added paramter for the file extenstion.

--- src/main/java/com/github/subh/code/clean/CodeClean.java
+++ src/main/java/com/github/subh/code/clean/CodeClean.java
@@ -2,13 +2,14 @@ package com.github.subh.code.clean;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.sonatype.inject.Parameters;
+import org.codehaus.plexus.util.StringUtils;
 
 /**
  * This Plugin will clean the whitespace and the
@@ -25,6 +26,9 @@ public class CodeClean extends AbstractMojo
     @Parameter(property="cleanDebug")
     private boolean cleanDebug;
 
+    @Parameter(property="extension", defaultValue="java")
+    private String extension;
+
     public void removeTabWithSpace(List<String> fileExtension) {
         CodeCleanUtil.replaceCharacter("\t", "    ", fileExtension, cleanDebug);
     }
@@ -34,7 +38,9 @@ public class CodeClean extends AbstractMojo
         logger.info("== invoking code cleaner ==");
 
         // file to process
-        List<String> filesToProcess = Arrays.asList("java", "xml", "json", "pom");
+        List<String> filesToProcess = Arrays.asList(extension.split(",")).stream()
+                .map(StringUtils::strip)
+                .collect(Collectors.toList());
 
         // replace all the tab character with the space character
         removeTabWithSpace(filesToProcess);
